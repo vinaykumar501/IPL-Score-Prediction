59c10b1cb627c323d8c61c857f01d7f32c17dbea
Sleep501 <badithimanivinay@gmail.com>
2026-02-22 13:12:37 +0530

    Refactor IPL score prediction app for team selection

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
 import streamlit as st
-import pickle  
+import pickle
 import numpy as np
+import pandas as pd
+
 model = pickle.load(open('model.pkl', 'rb'))
 transformer = pickle.load(open('transformer.pkl', 'rb'))
+
 st.title("IPL Score Prediction")
+
 teams = [
-    'Chennai Super Kings',
-    'Mumbai Indians',
-    'Royal Challengers Bangalore',
-    'Kolkata Knight Riders',
-    'Sunrisers Hyderabad',
-    'Delhi Capitals',
-    'Punjab Kings',
-    'Rajasthan Royals',
-    'Lucknow Super Giants',
-    'Gujarat Titans'
+'Sunrisers Hyderabad',
+'Royal Challengers Bangalore',
+'Mumbai Indians',
+'Rising Pune Supergiant',
+'Gujarat Lions',
+'Kolkata Knight Riders',
+'Kings XI Punjab',
+'Delhi Daredevils',
+'Chennai Super Kings',
+'Rajasthan Royals',
+'Deccan Chargers',
+'Kochi Tuskers Kerala',
+'Pune Warriors',
+'Rising Pune Supergiants',
+'Delhi Capitals'
 ]
+
 batting_team = st.selectbox("Batting Team", teams)
-bowling_team = st.selectbox("Bowling Team", teams)
+
+bowling_teams = [team for team in teams if team != batting_team]
+bowling_team = st.selectbox("Bowling Team", bowling_teams)
+
 current_score = st.number_input("Current Score", min_value=0)
-balls_left = st.number_input("Balls Left", min_value=0, max_value=120)
+balls_left = st.number_input("Balls Left", min_value=1, max_value=120)
 wickets_left = st.number_input("Wickets Left", min_value=0, max_value=10)
 current_run_rate = st.number_input("Current Run Rate", min_value=0.0)
+
 if st.button("Predict Score"):
-    import pandas as pd
+
     input_data = pd.DataFrame([{
         'batting_team': batting_team,
         'bowling_team': bowling_team,
@@ -32,6 +46,9 @@ if st.button("Predict Score"):
         'wickets_left': wickets_left,
         'current_run_rate': current_run_rate
     }])
+
     input_transformed = transformer.transform(input_data)
+
     prediction = model.predict(input_transformed)
+
     st.success(f"Predicted Final Score: {int(prediction[0])}")
